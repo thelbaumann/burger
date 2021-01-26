d63c11daf4b4c18fe4fa489a0c3a35bcd6fea51a
lbmann <l.bmann@yahoo.com>
2021-01-26 18:13:56 -0500

    debugging heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: 'localHost',
-  
-    port: 3306,
-  
-    user: 'root',
-  
-    password: 'root',
-  
-    database: 'burgers_db'
-  });
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    console.log("connected to JAWSDB_URL!");
+}
+else {
+
+    var connection = mysql.createConnection({
+        host: 'localHost',
+      
+        port: 3306,
+      
+        user: 'root',
+      
+        password: 'root',
+      
+        database: 'burgers_db'
+      });
+
+}
 
   connection.connect((err) => {
 
